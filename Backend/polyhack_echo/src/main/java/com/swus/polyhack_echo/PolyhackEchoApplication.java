1ec987ea62eb49f62358955aa553e9341c5ae60c
Yusun Choi <nsuy.ch@gmail.com>
2023-06-12 00:50:55 +0900

    chore: Initialize spring boot project (#2)

---
+++ Backend/polyhack_echo/src/main/java/com/swus/polyhack_echo/PolyhackEchoApplication.java
@@ -0,0 +1,13 @@
+package com.swus.polyhack_echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyhackEchoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyhackEchoApplication.class, args);
+    }
+
+}
