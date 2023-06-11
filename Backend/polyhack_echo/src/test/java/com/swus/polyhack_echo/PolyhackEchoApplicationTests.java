d6f7b5eda182393a8ddca3a4a0d3c717d384c55e
Yusun Choi <nsuy.ch@gmail.com>
2023-06-12 00:50:55 +0900

    chore: Initialize spring boot project (#2)

---
+++ Backend/polyhack_echo/src/test/java/com/swus/polyhack_echo/PolyhackEchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swus.polyhack_echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyhackEchoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
