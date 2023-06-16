1f44a0ae52b4938a3848a90565dbb58c4cb5183d
Yusun Choi <nsuy.ch@gmail.com>
2023-06-16 15:13:42 +0900

    feat: Add text formatting on Crawling Provider (#2)

--- Backend/polyhack_echo/src/main/java/com/swus/polyhack_echo/util/CrawlingProvider.java
+++ Backend/polyhack_echo/src/main/java/com/swus/polyhack_echo/util/CrawlingProvider.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +13,26 @@ import java.io.IOException;
 @Service
 @Slf4j
 public class CrawlingProvider {
+    private String formatText(Element element) {
+        StringBuilder formattedText = new StringBuilder();
+
+        for (Element child : element.children()) {
+            formattedText.append(child.text()).append("\n");
+            formattedText.append(formatText(child));
+        }
+
+        return formattedText.toString();
+    }
+
     private String getDataList(Document doc, String query) {
         Elements selects = doc.select(query);
+        String formattedText = null;
 
-        return selects.size() == 1 ? selects.get(0).text() : null;
+        if (selects.size() == 1) {
+            formattedText = formatText(selects.get(0));
+        }
+
+        return formattedText;
     }
 
     public String getNewsContent(String url, String query) throws IOException {
